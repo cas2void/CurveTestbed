744f46f56b297072ff4f8cc8081104f6a3f864af
cas2void <cas2void@gmail.com>
2022-04-14 19:16:29 +0800

    Add stubs

---
+++ Plugins/BufferPostProcessor/Source/BufferPostProcessor/Public/BufferPostPass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class BUFFERPOSTPROCESSOR_API FBufferPostPass
+{
+public:
+    FBufferPostPass();
+    ~FBufferPostPass();
+};
